474fd94446b0e4cb7a961e227a88264d5a4c03fb
Vikas Pandey <vik.pandey88@gmail.com>
2017-05-08 14:00:43 -0700

    updated multitheading 0.2

--- src/primaryData/TestApp.java
+++ src/primaryData/TestApp.java
@@ -12,9 +12,9 @@ import java.util.Map;
 public class TestApp {
 
 	public static void main(String[] args) {
-		
+
 		int number = 0;
-		
+
 		try {
 			int n = Integer.parseInt(args[0]);
 			number = n;
@@ -22,40 +22,40 @@ public class TestApp {
 			System.out.println("please enter a valid number ...");
 			e.printStackTrace();
 		}
-		
-		
-		
+
 		String filePath = args[1];
 		ProcessFiles process;
 		process = new ProcessFiles(new File(filePath));
 		ReadFiles read = new ReadFiles();
 		read.readProcessFile(process);
-		
+
 		startThread(read);
-		
+
 		Map<String, Integer> map = read.createMap();
 		MapSort mapSort = new MapSort(map);
 		System.out.println();
 		mapSort.printSorted(number);
 
 	}
-	
+
 	@SuppressWarnings("static-access")
 	public static void startThread(ReadFiles read) {
-		
-		int numOfThread = 10;
-	
+
+		int numOfThread = 100;
+
 		Thread[] thread = new Thread[numOfThread];
-		
-		for(int i = 1; i < numOfThread; i++) {
+
+		for (int i = 1; i < numOfThread; i++) {
 			thread[i] = new Thread(read);
 			thread[i].start();
+
 			try {
-				thread[i].sleep(100);
+				thread[i].sleep(1);
+				thread[i].join();
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+
 		}
 	}
 
